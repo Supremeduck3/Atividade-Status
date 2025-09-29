12702d37bac5a4b18f95670ac02c53603b9d796b
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-09-29 08:35:14 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+// Importar pacotes/bibliotecas
+import express from "express";
+import dotenv from "dotenv";
+import bruxosRoutes from "./routes/bruxosRoutes.js"
+
+const app = express();
+app.use(express.json());
+
+
+dotenv.config();
+const serverPort = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+    res.send("🚀 Servidor funcionando...");
+});
+
+app.use("/bruxos", bruxosRoutes)
+
+app.listen(serverPort, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
+});
